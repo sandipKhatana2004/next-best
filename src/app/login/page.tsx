e731a156b29e95fe151e9be9b0ad153c1a797bed
sandipKhatana2004 <sandipkhatana2004@gmail.com>
2023-07-27 16:29:57 +0530

    added some files configred project and made some pages and routes.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <div>
+            <h1>This will be Login page.</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignUp() {
+    return (
+        <div>
+            <h1>This will be SignUp page.</h1>
+        </div>
+    )
+}
