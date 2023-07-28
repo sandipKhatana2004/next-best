2ca4e10c3b2b0c6fe4e5660bb10ed5075519c12a
sandipKhatana2004 <sandipkhatana2004@gmail.com>
2023-07-28 20:01:36 +0530

    setting up structure for login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,39 @@
-export default function Login() {
-    return (
-        <div>
-            <h1>This will be Login page.</h1>
-        </div>
-    )
+"use client";
+import Link from "next/link";
+import React from "react";
+import {useRouter} from "next/navigation";
+import axios from "axios";
+
+export default function Loginpage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+  const onLogin = async () => {};
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+
+      <label htmlFor="email">email</label>
+      <input
+        className="p- border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({...user, email: e.target.value})}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p- border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => setUser({...user, password: e.target.value})}
+        placeholder="password"
+      />
+      <button onClick={onLogin}>SignUp here</button>
+    </div>
+  );
 }
